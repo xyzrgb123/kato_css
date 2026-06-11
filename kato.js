69b849e01f38eba6326c452c7e1727efffefe75d
xyzrgb123 <xyzrgb123@gmail.com>
2026-06-11 11:02:36 +0300

    добавил подсказки

--- kato.js
+++ kato.js
@@ -1,18 +1,22 @@
 // добавление класса active кликнутому элементу
 
-document.querySelectorAll('.toggle>*').forEach(el => {
-  el.addEventListener('click', () => {
-    el.classList.toggle('active');
-  });
+document.querySelectorAll('.group.clickable:not(.oo)>*').forEach(el => {
+  if (!el.hasAttribute('disabled')) {
+    el.addEventListener('click', () => {
+      el.classList.toggle('active');
+    });
+  }
 });
 
 // снять класс active с сестренского элемента и добавить кликнутому
-document.querySelectorAll('.toggle_oo>*').forEach(el => {
-  el.addEventListener('click', () => {
-    const active_bro = el.parentElement.querySelector('.active');
-    if (active_bro) {active_bro.classList.remove('active')};
-    el.classList.add('active');
-  });
+document.querySelectorAll('.group.clickable.oo>*').forEach(el => {
+  if (!el.hasAttribute('disabled')) {
+    el.addEventListener('click', () => {
+      const active_bro = el.parentElement.querySelector('.active');
+      if (active_bro) {active_bro.classList.remove('active')};
+      el.classList.add('active');
+    });
+  }
 });
 
 
