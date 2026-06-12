957c1770a79d02f9a03a6dbdb735d08a9d8ab557
xyzrgb123 <xyzrgb123@gmail.com>
2026-06-12 12:14:38 +0300

    добавил спойлер и jquery

--- !kato.js
+++ !kato.js
@@ -19,5 +19,45 @@ document.querySelectorAll('.group.clickable.oo>*').forEach(el => {
   }
 });
 
+// спойлер
+// // document.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('load', function() {
+
+
+// document.querySelectorAll('.spoiler').forEach(el => {
+    
+//     const height = el.nextElementSibling.clientHeight;
+//     // alert(height)
+//     el.nextElementSibling.style.setProperty('--spoiler', -height + 'px');
+//     el.addEventListener('click', () => {
+//       // const height = el.nextElementSibling.offsetHeight;
+//       // alert(height);
+//       // el.nextElementSibling.style.setProperty('--spoiler', -height + 'px');
+//       el.classList.toggle('active');
+//     });
+// });
+// });
+
+// const app = new VTTCue({
+// 	el: '#app',
+// 	data: {
+// 		name: 'mario'
+// 	},
+// 	methods: {
+// 		alertHandler() {
+// 			this.name = 'привет мир'
+// 			alert('привет мир')
+// 		}
+// 	},
+// })
+
+// $('body').addClass('test')
+
+$('.spoiler').next().slideUp();
+
+$('.spoiler').on("click", function() {
+  $(this).toggleClass('active');
+  $(this).next().slideToggle()
+});
 
 
